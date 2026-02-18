81cfe0c28978cad0cc1be0e3ad8d82a0710a174a
Ilan.G <ilang@flytrex.com>
2026-02-18 09:55:30 +0200

    1. Rename 'yaml_header' to 'payload_section_header'

--- dronecan_gui_tool/widgets/bus_monitor/transfer_decoder.py
+++ dronecan_gui_tool/widgets/bus_monitor/transfer_decoder.py
@@ -130,10 +130,10 @@ def get_payload_from_transfer(transfer, frames=None):
             # Reporting is best-effort; ignore failures
             pass
 
-    yaml_header = f"\nParsed payload:"
+    payload_section_header  = f"\nParsed payload:"
     yaml_dump = dronecan.to_yaml(transfer.payload)
 
-    parts = ['\n'.join(header), yaml_header, yaml_dump]
+    parts = ['\n'.join(header), payload_section_header , yaml_dump]
     return "\n".join(parts)
 
 
